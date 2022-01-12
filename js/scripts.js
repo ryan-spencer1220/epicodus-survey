96c316e38355884f9a91ade9359c5cc11decfb12
Ryan Spencer <ryan.spencer1220@gmail.com>
2022-01-12 13:22:18 -0800

    Edit JS file to display answers
    Co-Authored-By: Chase Stokes <chasehstokes@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,20 @@
 $(document).ready(function () {
-    $("form").submit(function (event) {
-        event.preventDefault();
-        const name = $("name").val();
-        $(".results").text(name);
-        const address = $("address").val();
-        $(".results").text(address);
-        const phone-number = $("phone-number").val();
-        $(".results").text(address);
-    })
-})
+  $("form").submit(function (event) {
+    event.preventDefault();
+    const name = $("#name").val();
+    $(".outputName").text(name);
+    const address = $("#address").val();
+    $(".outputAddress").text(address);
+    const phoneNumber = $("#phone-number").val();
+    $(".outputNumber").text(phoneNumber);
+    const beverage = $("#beverage").val();
+    $(".outputBeverage").text(beverage);
+    const favoriteColor = $("#color").val();
+    $(".outputColor").text(favoriteColor);
+    const dateOfBirth = $("#born").val();
+    $(".outputDOB").text(dateOfBirth);
+    const favoriteAnimal = $("input:radio[name=animal]:checked").val();
+    $(".outputAnimal").text(favoriteAnimal);
+    $(".results").show();
+  });
+});
